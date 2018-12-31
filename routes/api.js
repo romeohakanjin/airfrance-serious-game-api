000f3c4e50b3305820f08d8d4091a1976a9384d8
Sindy <sindy.lim@outlook.fr>
2018-12-31 15:25:11 +0100

    Modif passenger query dans api

--- routes/api.js
+++ routes/api.js
@@ -126,7 +126,8 @@ router.route('/aeroports/flights/:name/:terminal')
 
 router.route('/passenger/:num_flight')
 .get(function(req,res){ 
-        Aeroports.find({"flight": {"num_flight": req.params.num_flight}}, function(err, aeroports) {
+        Aeroports.find({"fligh.num_flight": req.params.num_flight}, function(err, aeroports) {
+        //Aeroports.find({"flight": {"num_flight": req.params.num_flight}}, function(err, aeroports) {
         if (err)
             res.send(err);
         res.json(aeroports);
