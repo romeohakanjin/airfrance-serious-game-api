20251adf03aad888efe9498391f5816f75bc6790
Sindy <sindy.lim@outlook.fr>
2019-01-02 13:07:44 +0100

    Modif passenger query dans api

--- routes/api.js
+++ routes/api.js
@@ -134,7 +134,7 @@ router.route('/aeroports/flights/:name/:terminal')
 // get all passengers by num_flight
 // Recherche bien avec le paramètre saisi, mais retourne tout l'objet aeoport
 // Trouver comment recupere juste les passagers
-router.route('/flight/passengers/:num_flight')
+router.route('/passengers/:num_flight')
 .get(function(req,res){ 
         Aeroports.find({"flight.num_flight": req.params.num_flight, 'flight.passenger': {$exists: true}}, function(err, aeroports) {
         if (err)
