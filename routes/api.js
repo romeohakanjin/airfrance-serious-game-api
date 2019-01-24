432ff23a908606340b31c0b575bb1114f4699a52
romeohakanjin <romeo.hakanjin@hotmail.fr>
2019-01-24 19:49:42 +0100

    flight arrival

--- routes/api.js
+++ routes/api.js
@@ -119,7 +119,7 @@ router.route('/airports/:name/:terminal')
 });
 
 // get the list of only aeroports registred for arrival
-router.route('/airports/arrival/:name')
+router.route('/airports/arrival/:name/:idid')
 .get(function(req,res){ 
         Aeroports.find({"flight.destination": req.params.name, 'flight': { $exists: false }}, function(err, aeroports) {
         if (err)
