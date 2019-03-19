9da6da7983b122c4c69d4d36292fe4ae32fea4ae
romeohakanjin <romeo.hakanjin@hotmail.fr>
2019-03-19 08:12:22 +0100

    update passenger incident

--- routes/api.js
+++ routes/api.js
@@ -242,11 +242,11 @@ router.route('/passenger/:last_name/:first_name')
 // update passenger incident
 router.route('/passengerIncident/:reference_number')
 .get(function(req,res){ 
-       /* Aeroports.update({ flight.passenger.reference_number : "6974" }, { $set: {flight.passenger.first_name: "Violette" }}, function(err, aeroports) {
-        if (err)
+        Aeroports.update({ "flight.passenger.reference_number" : "6974" }, { $set: {"flight.passenger.first_name" : "Violette" }}, function(err, aeroports) {
+        /*if (err)
             res.send(err);
-        res.json(aeroports);
-    });*/
+        res.json(aeroports);*/
+    });
 });
 
 
